b6446f321f0f4e8f7b81e65bfa71079e7d4789fb
Andy Harmon <andy.harmon@bbc.co.uk>
2022-02-01 14:23:53 +0000

    Show single question on index.js

--- client/src/QuizQuestion.js
+++ client/src/QuizQuestion.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import QuizQuesion from "./objects/QuizQuestion";
 
-function QuizJson(props) {
+function QuizQuestion(props) {
 
     const [ quizQuestion, setQuizQuestion ] = useState( { questionText: 'Hello world!' });
     
@@ -19,22 +19,22 @@ function QuizJson(props) {
                 console.log(jsonQuestion);
     
                 myQuestion = new QuizQuesion(jsonQuestion.question, jsonQuestion.answers, jsonQuestion.correct_answer);
+                console.log(myQuestion);
                 setQuizQuestion(myQuestion);
             });
     }, []);
 
     return(
         <div>
-            <p>{quizQuestion.questionText}</p>
+            <p className="question">{quizQuestion.questionText}</p>
             <ul>
                 <li>{quizQuestion.answers[0]}</li>
                 <li>{quizQuestion.answers[1]}</li>
                 <li>{quizQuestion.answers[2]}</li>
             </ul>
-
             <p>Correct answer is: {quizQuestion.correctAnswer}</p>
         </div>
     );
 }
 
-export default QuizJson;
+export default QuizQuestion;
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import QuizJson from './QuizJson';
+import QuizQuesion from './QuizQuestion';
 
 ReactDOM.render(
   <React.StrictMode>
-    <QuizJson />
+    <QuizQuesion />
   </React.StrictMode>,
   document.getElementById('root')
 );
